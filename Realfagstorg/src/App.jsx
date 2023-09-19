2659e6a85273087b647a32a2377bb0b8b463ad98
Fillip <wulfffil@gmail.com>
2023-09-19 14:22:24 +0200

    Main menu
    Created the design for the main menu, with a not working menu to the different subject pages.

--- Realfagstorg/src/App.jsx
+++ Realfagstorg/src/App.jsx
@@ -3,10 +3,38 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Realfags valgtorg</h1>
-      <p>Finn ditt realfag</p>
-    </>
+    <div id ="main">
+       <div id="valgmuligheter">
+        <a>
+          <button class="button-19"  role="button" >FYSIKK</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">BIOLOGI</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">KJEMI</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">INFORMASJONSTEKNOLOGI</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">TEKNOLOGI OF FORSKNINGSLÆRE</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">GEOFAG</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">MATEMATIKK FOR SAMFUNNSFAG</button>
+        </a>
+        <a>
+          <button class="button-19"  role="button">MATEMATIKK FOR REALFAG</button>
+        </a>
+      </div>
+      <div id="bakgrunn">
+        <h1>Realfag<br></br> valgtorg</h1>
+        <p>Trykk på faget du vil utforske</p>
+      </div>
+    </div>
   )
 }
 
