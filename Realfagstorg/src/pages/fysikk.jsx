0dc7bbe385cf2b51c7a8ab8cb93c93f771024258
isern <isjua001@osloskolen.no>
2023-11-23 08:57:20 +0100

    DifficultySelecter og CourseContent componenter

--- Realfagstorg/src/pages/fysikk.jsx
+++ Realfagstorg/src/pages/fysikk.jsx
@@ -10,6 +10,10 @@ const fysikk = () => {
         <div style={{ backgroundImage: `url(${yourImage})`, backgroundSize:"1400px", width:"100vw", height:"100vh", marginTop:"0px",padding:"0px"}}>
           <Overskrift valgtFag="Fysikk" image={yourImage}/>
       </div>
+      <blockquote cite="http://www.worldwildlife.org/who/index.html">
+For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.
+  </blockquote>
+
       <p>faef</p>
       <h1>Hei</h1>
       <h1>hei</h1>
